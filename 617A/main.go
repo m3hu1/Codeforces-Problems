6ed49a0f6f2a507c8a00e7a1bc6e37a69dd81fd8
Mehul Pathak <pathakkmehul@gmail.com>
2025-01-14 23:02:40 +0530

    Elephant

---
+++ 617A/main.go
@@ -0,0 +1,108 @@
+package main
+
+/*
+Codeforces Template (Go)
+Author: Mehul Pathak
+Version: 1.1
+*/
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	defer out.Flush()
+
+	// "Go"od luck!
+	x := readInt()
+
+	println((x + 4) / 5)
+}
+
+// cpp style typedefs
+type long = int64
+type float = float32
+type double = float64
+
+// faster i/o
+var in = bufio.NewReader(os.Stdin)
+var out = bufio.NewWriter(os.Stdout)
+
+// pythonic printing
+func print(arg ...interface{})            { fmt.Fprint(out, arg...) }
+func println(arg ...interface{})          { fmt.Fprintln(out, arg...) }
+func printf(f string, arg ...interface{}) { fmt.Fprintf(out, f, arg...) }
+
+func _scln() string {
+	ln, _ := in.ReadString('\n')
+	return strings.Trim(ln, " \r\n")
+}
+func _sc(s []string) string {
+	if len(s) == 0 {
+		return _scln()
+	}
+	return s[0]
+}
+
+func readLine() string              { return _scln() }
+func readString() string            { return _scln() }
+func readStrings() []string         { return strings.Split(_scln(), " ") }
+func readBool(s ...string) bool     { t, _ := strconv.ParseBool(_sc(s)); return t }
+func readByte(s ...string) byte     { t, _ := strconv.ParseUint(_sc(s), 10, 8); return byte(t) }
+func readDouble(s ...string) double { t, _ := strconv.ParseFloat(_sc(s), 64); return t }
+func readFloat(s ...string) float   { t, _ := strconv.ParseFloat(_sc(s), 32); return float32(t) }
+func readInt(s ...string) int       { t, _ := strconv.Atoi(_sc(s)); return t }
+func readLong(s ...string) long     { t, _ := strconv.ParseInt(_sc(s), 10, 64); return t }
+
+func readBools() []bool {
+	strs := readStrings()
+	arr := make([]bool, len(strs))
+	for i, s := range strs {
+		arr[i] = readBool(s)
+	}
+	return arr
+}
+func readBytes() []byte {
+	strs := readStrings()
+	arr := make([]byte, len(strs))
+	for i, s := range strs {
+		arr[i] = readByte(s)
+	}
+	return arr
+}
+func readDoubles() []double {
+	strs := readStrings()
+	arr := make([]double, len(strs))
+	for i, s := range strs {
+		arr[i] = readDouble(s)
+	}
+	return arr
+}
+func readFloats() []float {
+	strs := readStrings()
+	arr := make([]float, len(strs))
+	for i, s := range strs {
+		arr[i] = readFloat(s)
+	}
+	return arr
+}
+func readInts() []int {
+	strs := readStrings()
+	arr := make([]int, len(strs))
+	for i, s := range strs {
+		arr[i] = readInt(s)
+	}
+	return arr
+}
+func readLongs() []long {
+	strs := readStrings()
+	arr := make([]long, len(strs))
+	for i, s := range strs {
+		arr[i] = readLong(s)
+	}
+	return arr
+}
